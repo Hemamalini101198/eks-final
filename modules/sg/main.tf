f10e35b873eb3995e6b61eb05a84bc88dec051fd
Hemamalini101198 <150349833+Hemamalini101198@users.noreply.github.com>
2023-11-28 10:46:42 +0530

    Update main.tf

--- modules/sg/main.tf
+++ modules/sg/main.tf
@@ -6,7 +6,7 @@ resource "aws_security_group" "my-eks-sg" {
     from_port   = 0
     to_port     = 65535
     protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/16"]
+    cidr_blocks = [var.vpc_cidr]
   } 
 
   egress {
