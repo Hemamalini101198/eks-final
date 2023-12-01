1c3270285df8d0c58ebca44cec940b2c75592c36
Hemamalini101198 <150349833+Hemamalini101198@users.noreply.github.com>
2023-12-01 15:26:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,9 +6,9 @@ module "alb" {
   source           = "./modules/alb"
   alb_name         = var.alb_name
   vpc_cidr = var.cidr_block
-  public1_subnet_cidr = var.public1_subnet_id
-  public2_subnet_cidr = var.public2_subnet_id
-  sg_id = var.sg_id
+  public1_subnet_cidr = 
+  public2_subnet_cidr = 
+  sg_id = 
 }
 */
 module "eks_cluster" {
@@ -16,8 +16,8 @@ module "eks_cluster" {
   cluster_name  = var.cluster_name
   eks-node = "eks-managed-node"
   sg_id = var.sg_id
-  private1_subnet_id = var.private1_subnet_id
-  private2_subnet_id = var.private2_subnet_id
+  private1_subnet_cidr = var.private1_subnet_id
+  private2_subnet_cidr = var.private2_subnet_id
   public1_subnet_id =  var.public1_subnet_id
   public2_subnet_id = var.public2_subnet_id
   vpc_id        = var.vpc_id
