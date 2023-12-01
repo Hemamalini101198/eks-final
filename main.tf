34a6e5bf928f5656a3a9776180accf9fe1b5cda3
Hemamalini101198 <150349833+Hemamalini101198@users.noreply.github.com>
2023-12-01 12:56:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,49 +2,28 @@ provider "aws" {
   region = "ap-south-1" 
 }
 
-module "vpc" {
-  source               = "./modules/vpc"
-  vpc_name             = var.vpc_name
-  cidr_block           = var.cidr_block
-  public1_subnet_cidr  = var.public1_subnet_cidr
-  public2_subnet_cidr  = var.public2_subnet_cidr
-  private1_subnet_cidr = var.private1_subnet_cidr
-  private2_subnet_cidr = var.private2_subnet_cidr
-  project_name_env = var.project_name_env
-}
-
-module "sg" {
-  source               = "./modules/sg"
-  vpc_id = module.vpc.vpc_id  
-  project_name_env = var.project_name_env
-}
-
-
 module "alb" {
   source           = "./modules/alb"
   alb_name         = var.alb_name
   vpc_cidr = var.cidr_block
-  public1_subnet_cidr = module.vpc.public1_subnet_id
-  public2_subnet_cidr = module.vpc.public2_subnet_id
-  sg_id = module.sg.sg_vpc_id
-  depends_on = [module.vpc,module.eks_cluster, module.sg]
+  public1_subnet_cidr = var.public1_subnet_id
+  public2_subnet_cidr = var.public2_subnet_id
+  sg_id = var.sg_id
 }
-
 
 module "eks_cluster" {
   source        = "./modules/eks"
-  cluster_name  = "eks-cluster"
-  eks-node = var.eks_node_name
-  sg_id = module.sg.sg_vpc_id
-  private1_subnet_cidr = module.vpc.private1_subnet_id
-  private2_subnet_cidr = module.vpc.private2_subnet_id
-  public1_subnet_id =  module.vpc.public1_subnet_id
-  public2_subnet_id =  module.vpc.public2_subnet_id
-  vpc_id        = module.vpc.vpc_id
+  cluster_name  = var.cluster_name
+  eks-node = "eks-managed-node"
+  sg_id = var.sg_id
+  private1_subnet_cidr = var.private1_subnet_id
+  private2_subnet_cidr = var.private2_subnet_id
+  public1_subnet_id =  var.public1_subnet_id
+  public2_subnet_id = var.public2_subnet_id
+  vpc_id        = var.vpc_id
   desired_nodes = var.desired_nodes
   min_nodes = var.min_nodes
   max_nodes = var.max_nodes
   project_name_env = var.project_name_env
   
-  depends_on = [ module.vpc ]
 }
